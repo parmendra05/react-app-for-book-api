21ae041db08b69f14b6290ef4c46eac8db0e8d47
parmendra kumar <parmendrakumar718@gmail.com>
2023-10-15 20:53:28 +0530

    configured routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,25 @@ import HelloComponent from './Components/HelloComponent'
 import BookListComponent from './Components/BookListComponent'
 import HeaderComponent from './Components/HeaderComponent'
 import FooterComponent from './Components/FooterComponent'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       <HeaderComponent/>
-       <BookListComponent/>
-       <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent/>
+          <Routes>
+            {/* <BookListComponent> is enable for both end points 
+            http://localhost:3000/ & http://localhost:3000/employees */}
+            <Route path='/' element={<BookListComponent/>}></Route>
+            <Route path='/employees' element={<BookListComponent/>}></Route>
+          </Routes>
+       
+        <FooterComponent/>
+      </BrowserRouter>
+       
     </>
   )
 }
